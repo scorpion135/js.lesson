1a32b38dab8539e2bdf2b4747f247f9fbf482ecb
Maxim <max.curnosow2015@yandex.ru>
2022-12-06 16:40:12 +0300

    Это наш первый комит

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var number = 5;
+var string = "Hello";
+var symbol = Symbol();
+var boolean = true;
+undefined;
+null;
+
+/*
+let person = {
+    name: "Максим",
+    sex:"male",
+    age:"20",
+    city:"Moscow"
+};*/
+
+// let array = [3,5,6,7,8,9,0,1];
+// console.log(array[6]);
+// 
+// 
+// let answer = prompt("Сколько вам лет?");
+// 
+// if (answer < 18) {
+    // console.log("Вам отказано в посещении сайта")   
+// }
+// 
+// if (answer > 18) {
+    // console.log("Все хорошо - проходите")   
+// }
+// 
+
+// let answer = prompt("Ваше имя", "введите имя");
+// 
+
+
+
+
+
